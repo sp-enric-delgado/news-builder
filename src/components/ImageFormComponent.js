4d863f7f947b1796d9f4108ae4fbfcbe2847cbd0
sp-enric-delgado <enric.delgado@socialpoint.es>
2024-01-23 11:31:20 +0100

    imageformcomponent update

--- src/components/ImageFormComponent.js
+++ src/components/ImageFormComponent.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import '../styles/ImageFormComponent.css'
 
+/*
+    TO BE DONE:
+
+    - Check if input field has already loaded content - IF SO, REMOVE AND UPDATE -- useRef hook instead? => WON'T WORK WHEN TRYING TO LOAD THEM DYNAMICALLY
+    
+    - Create "Template Dropdown": dropdown that alows you to choose between different configurations
+    
+    - Dynamically add input fields depending on the loaded configuration 
+    
+    - Have placeholder images depending on the configuration
+*/
 
 function ImageFormComponent({OnCollectionUpdated}) {
 
-    const [imageCollection, updateImageCollection] = useState([]);
+    const [imageCollection, updateImageCollection] = useState({});
 
-    function handleImageUpload(event, isBackground = false){
+    function handleImageUpload(event){
         const uploadedImage = event.target.files[0];
+        const imageID = event.target.id;
+        
+        const newCollection = {
+            ...imageCollection,
+            [imageID]: uploadedImage,
+        };
 
-        updateImageCollection([...imageCollection, [uploadedImage, isBackground]]);
+        updateImageCollection(newCollection);
+        console.log("IMAGE COLLECTION: FORM COMPONENT\nCurrent collection: " + imageCollection);
     }
 
     return (
@@ -17,12 +35,12 @@ function ImageFormComponent({OnCollectionUpdated}) {
             <div className='fieldsSection'>
                 <div>
                     <h2>Background Image</h2>
-                    <input type="file" accept="image/*" onChange={(event) => handleImageUpload(event, true)} />
+                    <input id="background" type="file" accept="image/*" onChange={(event) => handleImageUpload(event, true)} />
                 </div>
 
                 <div>
                     <h2>Upload Images</h2>
-                    <input type="file" accept="image/*" onChange={handleImageUpload}/>
+                    <input id="uploaded-image-1" type="file" accept="image/*" onChange={handleImageUpload}/>
                 </div>
 
                 <div>
